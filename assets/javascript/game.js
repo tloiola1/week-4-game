dd134afeb7556a67165e509ec64920c75a2204b1
Tarciso <tarcisoloiola@hotmail.com>
2017-08-28 00:50:55 -0400

    game done

---
+++ assets/javascript/game.js
@@ -0,0 +1,78 @@
+clear();
+var myWin = 0;
+var pcWin = 0;
+
+  // Now for the hard part. Creating multiple crystals each with their own unique number value.
+
+  // We begin by expanding our array to include four options.
+  
+
+
+function clear(){
+    $("#pcNumber").empty();
+    $("#myScore").html(0);
+    counter = 0;
+    targetNumber = Math.floor(Math.random() * (120 - 19) + 19);
+    $("#pcNumber").html(targetNumber);
+      
+    var numberOptions =[];
+
+    for (var i = 0; i < 12; i++){
+        numberOptions[i] = i+1;
+    }
+console.log(numberOptions);
+    var a, b, i;
+    for (i = numberOptions.length; i; i--) {
+        a = Math.floor(Math.random() * i);
+        b = numberOptions[i - 1];
+        numberOptions[i - 1] = numberOptions[a];
+        numberOptions[a] = b;
+    }
+console.log(numberOptions);
+ 
+
+    var imageCrystal = $("#blue");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("data-crystalvalue", numberOptions[0]);
+
+    imageCrystal = $("#yellow");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("data-crystalvalue", numberOptions[1]);
+
+    imageCrystal = $("#pink");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("data-crystalvalue", numberOptions[2]);
+    
+    imageCrystal = $("#green");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("data-crystalvalue", numberOptions[3]);
+
+}
+
+
+  $(".crystal-image").on("click", function() {
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+
+    counter += crystalValue;
+
+    $("#myScore").html(counter);
+
+
+    if (counter == targetNumber) {
+      myWin++;
+      $("#wins").html(myWin);
+      alert("You Win!!"+" You Score"+counter);
+      clear();
+
+    }
+
+    else if (counter >= targetNumber) {
+      pcWin++;
+      $("#losses").html(pcWin);
+      alert("You loose!!"+" You Score"+counter);
+      clear();
+
+    }
+
+  });
